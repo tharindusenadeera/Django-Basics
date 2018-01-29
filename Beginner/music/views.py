dcccae94d8b06527be74039355b9455c880e5502
Tharindu <tharindusenadeera081@gmail.com>
2018-01-30 00:39:22 +0530

    Initial Commit

--- Beginner/music/views.py
+++ Beginner/music/views.py
@@ -3,3 +3,7 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("<h1>This is the music app homepage</h1>")
+
+
+def detail(request, album_id):
+    return HttpResponse("<h2>This page shows details about music id " + str(album_id) + "</h2>")
